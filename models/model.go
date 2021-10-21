02605b9b4af95bb720f87aa82cee6d805a912b5b
codingbing <codingbing@163.com>
2021-10-21 10:17:14 +0800

    minor:test action load config

--- models/model.go
+++ models/model.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"helloadmin/config"
 	"log"
 )
 
@@ -15,15 +16,15 @@ func init() {
 		dbName, user, password, host string
 	)
 
-	//cfg, err := config.Load().GetSection("database")
+	cfg, err := config.Load().GetSection("database")
 	if err != nil {
 		fmt.Println("Fail to get section 'database': %v", err)
 	}
 
-	//dbName = cfg.Key("NAME").String()
-	//user = cfg.Key("USER").String()
-	//password = cfg.Key("PASSWORD").String()
-	//host = cfg.Key("HOST").String()
+	dbName = cfg.Key("NAME").String()
+	user = cfg.Key("USER").String()
+	password = cfg.Key("PASSWORD").String()
+	host = cfg.Key("HOST").String()
 	//tablePrefix = cfg.Key("TABLE_PREFIX").String()
 
 	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
